d2470783e26e3904f77978d79d86e4c406902722
DenFlyvendeGed <simont1998@gmail.com>
2025-04-11 13:27:42 +0200

    Updated validations

--- schemas/Name.ts
+++ schemas/Name.ts
@@ -3,6 +3,6 @@ import * as v from "valibot";
 const NAME_MAX_LENGTH = 30;
 
 
-export const NameSchema = v.pipe(v.string(), v.length(NAME_MAX_LENGTH));
+export const NameSchema = v.pipe(v.string(), v.maxLength(NAME_MAX_LENGTH));
 
 export type Name = v.InferOutput<typeof NameSchema>;
--- schemas/Uuid.ts
+++ schemas/Uuid.ts
@@ -1,5 +1,5 @@
 import * as v from "valibot";
 
-export const UuidSchema = v.pipe(v.string(), v.uuid());
+export const UuidSchema = v.pipe(v.string(), v.length(24));
 
 export type Uuid = v.InferOutput<typeof UuidSchema>;

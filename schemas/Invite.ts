c5005649b5f1ff0be336bc6e1c168e515c5f5f37
DenFlyvendeGed <simont1998@gmail.com>
2025-04-11 10:57:53 +0200

    Added invite

---
+++ schemas/Invite.ts
@@ -0,0 +1,9 @@
+import * as v from 'valibot';
+import { UuidSchema } from './Uuid';
+
+export const InviteSchema = v.object({
+    user: UuidSchema,
+    group: UuidSchema,
+});
+
+export type Invite = v.InferOutput<typeof InviteSchema>;

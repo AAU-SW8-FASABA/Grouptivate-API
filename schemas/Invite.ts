45251b33e408a4cdf9a4cf9fb80ac49483c11e92
Frydensbjerg <christoffernoertoft@gmail.com>
2025-04-17 11:09:51 +0200

    Invite tilføjet invitee

--- schemas/Invite.ts
+++ schemas/Invite.ts
@@ -4,6 +4,7 @@ import { UuidSchema } from "./Uuid";
 export const InviteSchema = v.object({
   user: UuidSchema,
   group: UuidSchema,
+  invitee: UuidSchema
 });
 
 export type Invite = v.InferOutput<typeof InviteSchema>;

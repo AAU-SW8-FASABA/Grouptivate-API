ef986b86d0fffa174154fe27544796a2da89dd8f
Anton <dev@atjn.dk>
2025-04-22 08:20:52 +0200

    Intial concept

--- schemas/User.ts
+++ schemas/User.ts
@@ -1,11 +1,39 @@
 import * as v from "valibot";
 import { UuidSchema } from "./Uuid";
 import { NameSchema } from "./Name";
+import { RequestSchema } from "../containers/Request";
 
 export const UserSchema = v.object({
   uuid: UuidSchema,
   name: NameSchema,
   groups: v.array(UuidSchema),
 });
-
 export type User = v.InferOutput<typeof UserSchema>;
+
+/**
+ * REST methods
+ */
+
+const UserCreateRequestBodySchema = v.pick(UserSchema, ["name"]);
+const UserCreateResponseBodySchema = v.pick(UserSchema, ["uuid"]);
+export const UserCreateRequestSchema: RequestSchema<
+  undefined,
+  typeof UserCreateRequestBodySchema,
+  typeof UserCreateResponseBodySchema
+> = {
+  requestBody: UserCreateRequestBodySchema,
+  responseBody: UserCreateResponseBodySchema,
+};
+
+const UserGetSearchParamsSchema = {
+  uuid: v.pick(UserSchema, ["uuid"]),
+};
+const UserGetResponseBodySchema = v.omit(UserSchema, ["uuid"]);
+export const UserGetRequestSchema: RequestSchema<
+  typeof UserGetSearchParamsSchema,
+  undefined,
+  typeof UserGetResponseBodySchema
+> = {
+  searchParams: UserGetSearchParamsSchema,
+  responseBody: UserGetResponseBodySchema,
+};

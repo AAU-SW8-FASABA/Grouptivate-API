9c694eacca0d80227efa6f8d25b83e8ecbf2b819
Anton <dev@atjn.dk>
2025-04-22 13:10:58 +0200

    Update Invite types

--- schemas/Invite.ts
+++ schemas/Invite.ts
@@ -1,9 +1,47 @@
 import * as v from "valibot";
 import { UuidSchema } from "./Uuid";
+import { UserSchema } from "./User";
+import type { RequestSchema } from "../containers/Request";
 
 export const InviteSchema = v.object({
-  user: UuidSchema,
+  uuid: UuidSchema,
   group: UuidSchema,
+  invited: UuidSchema,
+  invitee: UuidSchema,
 });
-
 export type Invite = v.InferOutput<typeof InviteSchema>;
+
+/**
+ * REST methods
+ */
+
+const InviteCreateSearchParamsSchema = {
+  user: v.pick(UserSchema, ["uuid"]),
+};
+const InviteCreateRequestBodySchema = v.pick(InviteSchema, [
+  "group",
+  "invited",
+]);
+export const InviteCreateRequestSchema: RequestSchema<
+  typeof InviteCreateSearchParamsSchema,
+  typeof InviteCreateRequestBodySchema,
+  undefined
+> = {
+  searchParams: InviteCreateSearchParamsSchema,
+  requestBody: InviteCreateRequestBodySchema,
+  responseBody: undefined,
+};
+
+const InvitesGetSearchParamsSchema = {
+  user: v.pick(UserSchema, ["uuid"]),
+};
+const InvitesGetResponseBodySchema = v.array(v.omit(InviteSchema, ["invited"]));
+export const InviteGetRequestSchema: RequestSchema<
+  typeof InvitesGetSearchParamsSchema,
+  undefined,
+  typeof InvitesGetResponseBodySchema
+> = {
+  searchParams: InvitesGetSearchParamsSchema,
+  requestBody: undefined,
+  responseBody: InvitesGetResponseBodySchema,
+};

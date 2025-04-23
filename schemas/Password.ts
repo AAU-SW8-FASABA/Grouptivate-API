cc318b43e74eb7efaee0fa141839ec73fb284cf1
Anton <dev@atjn.dk>
2025-04-23 13:35:07 +0200

    Password.ts whoopsie

---
+++ schemas/Password.ts
@@ -0,0 +1,12 @@
+import * as v from "valibot";
+
+export const PasswordSchema = v.pipe(
+  v.string(),
+  v.minLength(8, "Your password is too short."),
+  v.maxLength(30, "Your password is too long."),
+  v.regex(/[a-z]/, "Your password must contain a lowercase letter."),
+  v.regex(/[A-Z]/, "Your password must contain a uppercase letter."),
+  v.regex(/[0-9]/, "Your password must contain a number."),
+);
+
+export type Password = v.InferOutput<typeof PasswordSchema>;

28d613a75133a2c45cffa56ae811a5073e682198
Anton <dev@atjn.dk>
2025-04-25 11:13:52 +0200

    Taking a big fat dump on bastians code

--- schemas/Login.ts
+++ schemas/Login.ts
@@ -1,5 +1,4 @@
 import * as v from 'valibot';
-import { UuidSchema } from './Uuid';
 import { NameSchema } from './Name';
 import { PasswordSchema } from './Password';
 import type { RequestSchema } from '../containers/Request';
@@ -8,16 +7,10 @@ import type { RequestSchema } from '../containers/Request';
  * REST methods
  */
 
-export const TokenSchema = v.object({
-    token: v.string(),
-});
-export type Token = v.InferOutput<typeof TokenSchema>;
-
 const LoginRequestBodySchema = v.object({
     name: NameSchema,
     password: PasswordSchema,
 });
-
 export const LoginRequestSchema: RequestSchema<
     Record<never, never>,
     typeof LoginRequestBodySchema,
